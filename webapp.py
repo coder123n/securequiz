b13e850c5635c4bdd262748b65d849947d8d35a7
coder123n <naomisellis@gmail.com>
2020-01-30 09:07:27 -0800

    thurs

--- webapp.py
+++ webapp.py
@@ -36,7 +36,10 @@ def renderPage2():
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    session["favorite_color"] = request.form["favoriteColor"]
+    if request.form["pres"] == "Abe":
+        session["Q2"] = True
+    else:
+        session["Q2"] = False
     return render_template('page3.html')
     
 @app.route('/page4',methods=['GET','POST'])
